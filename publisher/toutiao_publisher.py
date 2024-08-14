7040d9db2f3752fefac8051400b3c2acd7d0a214
ken <ken.li@sacmigroup.com>
2024-08-14 16:43:24 +0800

    完善头条发布脚本

--- publisher/toutiao_publisher.py
+++ publisher/toutiao_publisher.py
@@ -35,10 +35,22 @@ from utils.yaml_file_utils import (
 )
 
 
+def set_title(driver, publish_data):
+    title = driver.find_element(
+        By.XPATH,
+        '//div[@class="publish-editor-title-inner"]//textarea[contains(@placeholder,"请输入文章标题")]',
+    )  # 找到文章标题元素
+    title.clear()  # 清空标题内容
+
+    title.send_keys(publish_data.title)  # 设置标题内容
+    exponential_sleep(1)  # 等待1秒
+
+
 def close_ai_assistant(driver):
     # 随机选择位方法
     # if random.choice([True, False]):
-    close_button = driver.find_element(By.CSS_SELECTOR, "h1.header svg.close-btn")
+    close_button = driver.find_element(
+        By.CSS_SELECTOR, "h1.header svg.close-btn")
     # else:
     #     close_button = driver.find_element(
     #         By.XPATH,
@@ -50,50 +62,21 @@ def close_ai_assistant(driver):
     actions.move_to_element(close_button).perform()
 
     # 随机等待
-    exponential_sleep(2)  # 等待2秒
+    exponential_sleep(1)  # 等待1秒
 
     # 点击
     close_button.click()
 
     # 随机等待
-    exponential_sleep(2)  # 等待2秒
+    exponential_sleep(1)  # 等待1秒
 
 
-def toutiao_publisher(driver, publish_data: PublishData):
-    toutiao_config = read_toutiao()  # 读取头条配置
-    common_config = read_common()  # 读取通用配置
-
-    auto_publish = common_config["auto_publish"]  # 是否自动发布
-
-    driver.switch_to.new_window("tab")  # 打开新标签页并切换到新标签页
-    driver.get(toutiao_config["site"])  # 浏览器实例现在可以被重用，进行你的自动化操作
-    exponential_sleep(2)  # 等待2秒
-
-    wait_login(
-        driver,
-        By.XPATH,
-        '//div[@class="publish-editor-title-inner"]//textarea[contains(@placeholder,"请输入文章标题")]',
-    )  # 等待用户登录
-
-    # 标题设置
-    title = driver.find_element(
-        By.XPATH,
-        '//div[@class="publish-editor-title-inner"]//textarea[contains(@placeholder,"请输入文章标题")]',
-    )  # 找到文章标题元素
-    title.clear()  # 清空标题内容
-
-    title.send_keys(publish_data.title)  # 设置标题内容
-    exponential_sleep(2)  # 等待2秒
-
-    close_ai_assistant(driver)  # 关闭 头条创作助手
-
-    # 正文内容设置
+def set_body(driver, publish_data):
     content_file_html = convert_content_to_html(
         publish_data.content, False
     )  # 将文章内容转成 HTML
 
     get_html_web_content(driver, content_file_html)  # 获取 HTML 内容
-    exponential_sleep(2)  # 等待2秒
 
     driver.switch_to.window(driver.window_handles[-1])  # 切换到新标签页(这一步可能多余)
     exponential_sleep(1)  # 等待1秒
@@ -104,7 +87,7 @@ def toutiao_publisher(driver, publish_data: PublishData):
         driver
     )  # 模拟实际的粘贴操作（在某些情况下可能更合适）：
     # action_chains.key_down(Keys.TAB).key_up(Keys.TAB).perform()
-    # exponential_sleep(2)  # 等待2秒
+    # exponential_sleep(1)  # 等待1秒
     # print(pyperclip.paste())
 
     content_element = driver.find_element(
@@ -116,46 +99,150 @@ def toutiao_publisher(driver, publish_data: PublishData):
     action_chains.key_down(cmd_ctrl).send_keys("v").key_up(
         cmd_ctrl
     ).perform()  # 粘贴正文内容
-    exponential_sleep(3)  # 等待3秒
+    exponential_sleep(1)  # 等待1秒
 
-    # # 展示封面
-    # # TODO
 
-    # # 摘要
-    # summary = common_config["summary"]
-    # if summary:
-    #     summary_input = driver.find_element(
-    #         By.XPATH,
-    #         '//div[@class="multi-abstract-cell-content-input"]//textarea[contains(@placeholder,"好的摘要比标题更吸引读者")]',
-    #     )
-    #     summary_input.send_keys(summary)
-    # exponential_sleep(2)  # 等待2秒
-
-    # # 投放广告
-    # # TODO
-
-    # # 原创首发
-    # 声明首发
-    original_button = driver.find_element(
-        By.XPATH,
-        '//div[contains(@class, "exclusive-checkbox-wraper")]//label[contains(@class, "byte-checkbox")]//input[@type="checkbox"]',
+def select_single_title(driver):
+    # 使用显式等待
+    title_wait = WebDriverWait(driver, 10)
+    label_element = title_wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//span[contains(@class, "byte-radio-inner-text") and text()="单标题"]/parent::span'))
     )
-    if not original_button.is_selected():
-        original_button.click()
+
+    # 滚动页面到元素可见位置
+    driver.execute_script("arguments[0].scrollIntoView(true);", label_element)
+    exponential_sleep(1)  # 等待1秒
+
+    driver.execute_script("window.scrollBy(0, -100);")    # 稍微向上滚动一点，确保元素没有被遮挡
+    exponential_sleep(1)  # 等待1秒
+
+    label_element.click()
     exponential_sleep(2)  # 等待2秒
+
+
+def select_no_cover(driver):
+    # 使用显式等待
+    element_wait = WebDriverWait(driver, 10)
+    label_element = element_wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//span[contains(@class, "byte-radio-inner-text") and text()="无封面"]/parent::span'))
+    )
+
+    # driver.execute_script(
+    #     "arguments[0].scrollIntoView(true);", label_element)  # 滚动页面到元素可见位置
+    # exponential_sleep(1)  # 等待1秒
+
+    # driver.execute_script("window.scrollBy(0, -100);")    # 稍微向上滚动一点，确保元素没有被遮挡
+    # exponential_sleep(1)  # 等待1秒
+
+    label_element.click()
+    exponential_sleep(1)  # 等待1秒
+
+
+def declare_first_publication():
+    exponential_sleep(0.5)  # 用于函数占位，后续可以删除
+
+    # # 使用显式等待
+    # wait = WebDriverWait(driver, 10)
+    # label_element = wait.until(
+    #     EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "exclusive-checkbox-wraper")]//span[contains(text(), "头条首发")]'))
+    # )
+
+    # # 滚动页面到元素可见位置
+    # driver.execute_script("arguments[0].scrollIntoView(true);", label_element)
+    # exponential_sleep(1)  # 等待1秒
+
+    # driver.execute_script("window.scrollBy(0, -100);")    # 稍微向上滚动一点，确保元素没有被遮挡
+    # exponential_sleep(1)  # 等待1秒
+
+    # # 找到对应的<input>元素 TODO：搜索不到该 input
+    # input_element = label_element.find_element(By.XPATH, './/input')
+
+    # # 检查<input>元素是否已选中
+    # is_checked = input_element.get_attribute('checked') == 'true'
+    # # 如果未勾选，则点击元素
+    # if not is_checked:
+    #     print("click")
+    #     # 点击包含文本 "头条首发" 的 <span> 元素
+    #     label_element.click()
+    #     exponential_sleep(1)  # 等待1秒
 
     # original_button = driver.find_element(
     #     By.XPATH, '//div[@class="original-tag"]//span[contains(text(),"声明原创")]'
     # )
     # original_button.click()
-    # exponential_sleep(2)  # 等待2秒
+    # exponential_sleep(1)  # 等待1秒
 
-    # # 合集
-    # # TODO
 
-    # # 发布
-    # if auto_publish:
-    #     publish_button = driver.find_element(
-    #         By.XPATH, '//div[contains(@class,"publish-btn-last")]'
-    #     )
-    #     publish_button.click()
+def select_work_declaration(driver):
+    # 使用显式等待
+    title_wait = WebDriverWait(driver, 10)
+    label_element = title_wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//span[contains(@class, "byte-checkbox-inner-text") and text()="取材网络"]/parent::span'))
+    )
+
+    # 滚动页面到元素可见位置
+    driver.execute_script("arguments[0].scrollIntoView(true);", label_element)
+    exponential_sleep(2)  # 等待2秒
+
+    label_element.click()
+    exponential_sleep(1)  # 等待1秒
+
+
+def toutiao_publisher(driver, publish_data: PublishData):
+    toutiao_config = read_toutiao()  # 读取头条配置
+    common_config = read_common()  # 读取通用配置
+
+    auto_publish = common_config["auto_publish"]  # 是否自动发布
+
+    driver.switch_to.new_window("tab")  # 打开新标签页并切换到新标签页
+    driver.get(toutiao_config["site"])  # 浏览器实例现在可以被重用，进行你的自动化操作
+    exponential_sleep(1)  # 等待1秒
+
+    wait_login(
+        driver,
+        By.XPATH,
+        '//div[@class="publish-editor-title-inner"]//textarea[contains(@placeholder,"请输入文章标题")]',
+    )  # 等待用户登录
+
+    # 标题设置：
+    set_title(driver, publish_data)
+
+    # 关闭 头条创作助手：
+    close_ai_assistant(driver)
+
+    # 设置正文内容：
+    set_body(driver, publish_data)
+
+    # 标题设置：
+    select_single_title(driver)  # 单标题
+
+    # 展示封面：
+    select_no_cover(driver)  # 无封面
+
+    # 投放广告：
+    # 可默认勾选
+
+    # 声明首发 头条首发：
+    # declare_first_publication() // TODO：暂时注释，因为函数功能不完善，且头条能默认勾选首发
+
+    # 合集：
+    # TODO
+
+    # 同时发布微头条：
+    # 可默认勾选
+
+    # 作品声明：
+    select_work_declaration(driver)  # 取材网络
+
+    # 发布：
+    if auto_publish:
+        print(1)
+        publish_button = driver.find_element(
+            By.XPATH, '//div[contains(@class,"publish-btn-last")]'
+        )
+        publish_button.click()
+    else:
+        print(2)
